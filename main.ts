af85786cc6370c10fdfd3ffd9c0cca9d0382d2f2
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-26 15:57:45 +0000

    Updated blocks

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 radio.setGroup(255)
 basic.forever(function () {
     if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Fire1)) {
-        radio.sendValue("forwards", 30)
+        radio.sendValue("forwards", 50)
     } else if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Fire2)) {
-        radio.sendValue("back", 30)
+        radio.sendValue("back", 50)
+    } else {
+        radio.sendValue("stop", 0)
     }
 })
